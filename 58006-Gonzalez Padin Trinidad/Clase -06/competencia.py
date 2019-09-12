9959f513a6c075540fc20110de53f362223e6c3c
trini-gonzalezpadin <trinigonzalezpadin@gmail.com>
2019-09-12 19:09:08 -0300

    trinu

---
+++ 58006-Gonzalez Padin Trinidad/Clase -06/competencia.py
@@ -0,0 +1,23 @@
+from generador import generador,generador_mejor
+buscado=generador(1,100000)
+nadie_adivino=True
+listanumero=[]
+while nadie_adivino:
+    print("Ingrese un numero")
+    numero=int(input())
+    if numero == buscado:
+            print("Ganaste ,fin del juego")
+            nadie_adivino=False
+    if numero < buscado :
+            print("El numero es mayor")
+    if numero > buscado:
+            print("El numero es mayor")
+    if nadie_adivino==True:
+        numero2=generador_mejor(1,100000,listanumero)
+        print("El numero que penso la compu en su turno fue:" + str(numero2))
+        listanumero.append(numero2)
+        if numero2==buscado:
+            print("Gano la compu")
+            nadie_adivino=False
+    
+
--- 58006-Gonzalez Padin Trinidad/Clase -06/generador.py
+++ 58006-Gonzalez Padin Trinidad/Clase -06/generador.py
@@ -1,5 +1,14 @@
 import random
 def generador(minimo,maximo):
     return random.randint(minimo,maximo)
-if __name__ == "__main__":
-    print(generador(1,20))
+#if __name__ == "__main__":
+    #print(generador(1,20))
+
+def generador_mejor(minimo,maximo,lista):
+    encontrado= True
+    while encontrado:
+        aleatorio= random.randint(minimo,maximo)
+        encontrado=False 
+        if aleatorio in lista:
+            encontrado=True 
+    return aleatorio 

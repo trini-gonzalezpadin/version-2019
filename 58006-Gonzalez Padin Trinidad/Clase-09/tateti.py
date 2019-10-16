25ce2bbd1d15fd8e0081d97e05a6ce31851d26c5
trini-gonzalezpadin <trinigonzalezpadin@gmail.com>
2019-10-16 19:53:30 -0300

    Trinu

---
+++ 58006-Gonzalez Padin Trinidad/Clase-09/tateti.py
@@ -0,0 +1,39 @@
+from tablero import draw_tablero,full,validate,win
+numeros=[]
+tablero=[]
+for i in range(0,9):
+    tablero.append(" ")
+    numeros.append(str(i+1))
+draw_tablero(numeros)
+while not win(tablero) and not full(tablero):
+    print("Es el turno de la x")
+    valido=False
+    while not valido:
+        print("Ingrese una posicion valida de 1 a 9 (x)")
+        posicion=int(input())
+        valido=validate (tablero,posicion)
+        if not valido:
+            print("Error de poscion")
+    tablero[posicion-1]=numeros[posicion-1]="x"
+    draw_tablero(numeros)
+    gano=win(tablero)
+    if gano:
+        print("Gano la x")
+    else: 
+        print("Es el turno de la o")
+        valido=False
+        while not valido:
+            print("Ingrese una posicion valida de 1 a 9 (o)")
+            posicion=int(input())
+            valido=validate (tablero,posicion)
+            if not valido:
+                print("Error de poscion")
+        tablero[posicion-1]=numeros[posicion-1]="o"
+        draw_tablero(numeros)
+        gano=win(tablero)
+        if gano:
+            print("Gano la o")
+if full(tablero) and not win(tablero):
+    print("Nadie gano")
+
+
